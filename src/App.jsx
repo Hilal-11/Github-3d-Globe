7e95bf6426b3a4fe3ee5a62450f1b0ecadaa89f2
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-13 19:30:49 +0530

    github 3d globe

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { GlobeDemo } from './Components/GithubGlobe'
+function App() {
+  return (
+    <div>
+      <GlobeDemo />
+    </div>
+  )
+}
+
+export default App
